28feca4dc8ec6ba9103807f8d44c2d453c8223f7
Linda-Mensah <lindaakmensah@gmail.com>
2025-07-16 14:36:52 +0000

    protected route update

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,33 @@
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // check localStorage or make an API call here
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthContext;
+export const useAuth = () => useContext(AuthContext);
