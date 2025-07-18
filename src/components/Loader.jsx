6ce51cf2b100aa8eff7e4c79b3782bd67070e0b1
Linda-Mensah <lindaakmensah@gmail.com>
2025-07-18 09:51:17 +0000

    package

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@
 const Loader = () => {
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-64px)]">
-      <div className="w-12 h-12 border-4 border-green-600 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-12 h-12 border-4 border-green-400 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
